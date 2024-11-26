bd9d748e65458e7d455997dcc55d04d2c0bc1679
njeanfre <nangy.pro@gmail.com>
2024-11-26 14:13:03 +0100

    css

--- js/effet_print.js
+++ js/effet_print.js
@@ -2,7 +2,6 @@
   // Textes à afficher
   const textH1 = "Bienvenue sur mon portfolio"; // Texte pour le H1
   const textH2 = "Développeur full stack\nJe m'appelle Jean-Frédéric Nangy"; // Texte pour le H2
-  const textP = "Je suis un développeur full stack passionné par la création de projets divers.\nJ'ai commencé à coder il y a 1 an et demi lorsque j'ai intégré l'école Zone01\net j'ai travaillé sur de nombreux projets depuis.\nJe suis spécialisé dans le développement back-end et front-end.\nVoici quelques langages et technologies avec lesquelles je travaille :";
 
   // Paramètres
   const typingSpeed = 15; // Vitesse d'écriture en millisecondes
@@ -10,7 +9,6 @@
   // Cible les éléments HTML
   const typingEffectH1 = document.getElementById("typing-effect-h1");
   const typingEffectH2 = document.getElementById("typing-effect-h2");
-  const typingEffectP = document.getElementById("typing-effect-p");
 
   // Fonction pour afficher progressivement le texte
   function typeText(element, text, callback) {
@@ -32,7 +30,5 @@
 
   // Lance l'effet d'écriture pour H1, puis H2
   typeText(typingEffectH1, textH1, () => {
-    typeText(typingEffectH2, textH2, () => {
-      typeText(typingEffectP, textP);
-    });
+    typeText(typingEffectH2, textH2);
   });
