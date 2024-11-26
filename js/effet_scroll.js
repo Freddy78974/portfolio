6b324b0ae9a55f232b707b4cb54d8a667cfa5a36
njeanfre <nangy.pro@gmail.com>
2024-11-26 10:33:15 +0100

    ajout d'effet

--- js/effet_scroll.js
+++ js/effet_scroll.js
@@ -19,3 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".container-photo");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // L'élément entre dans la vue
+          entry.target.classList.add("visible");
+        } else {
+          // L'élément sort de la vue
+          entry.target.classList.remove("visible");
+        }
+      });
+    });
+  
+    // Observez chaque section
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
